05345bb8dbb6d6b110c218f62ca80143d2c80609
lindMT <103123352+lindMT@users.noreply.github.com>
2024-07-30 17:22:01 +0800

    delete users

--- controllers/adminhomeController.js
+++ controllers/adminhomeController.js
@@ -37,7 +37,6 @@ const adminhomeController = {
                 if(req.session.user.userType === "admin") {
                     
                     const postID = req.params.postID;
-                    console.log('NIGGA ' + postID);
                     
                     Post.deletePost(postID, (err, results) => {
                         if (err) {
@@ -60,6 +59,36 @@ const adminhomeController = {
         }
     },
 
+    
+    deleteUser: function (req, res){
+        try{
+            console.log('Session data from delete user:', req.session); 
+            // hjopw do i ensure that it is indeed getting req.session
+            if(req.session.isLoggedIn){
+                if(req.session.user.userType === "admin") {
+                    
+                    const userID = req.params.userID;
+                    
+                    User.deleteUser(userID, (err, results) => {
+                        if (err) {
+                        console.log(err);
+                        return res.status(500).send("Database error");
+                        }
+                        res.redirect('/admin_home');
+                    });
+
+                    
+                } else{
+                    res.redirect("/logout");
+                }
+            } else {
+                res.redirect("/");
+            }
+        } catch(e){
+            console.log('Error (Admin) HERE');
+            res.redirect("/logout");
+        }
+    },
 };
 
 module.exports = adminhomeController;
